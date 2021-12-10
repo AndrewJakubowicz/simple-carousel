c0cc8b45d31ca34625f09967c77cba26fc558c22
Andrew Jakubowicz <ajakubowicz@google.com>
2021-12-09 23:20:30 -0800

    Part 3 - Slots and slots stuff

--- src/simple-carousel.ts
+++ src/simple-carousel.ts
@@ -4,20 +4,59 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { LitElement, html, css } from 'lit';
+import {
+  customElement,
+  property,
+  queryAssignedNodes,
+} from 'lit/decorators.js';
 
 @customElement('simple-carousel')
 export class SimpleCarousel extends LitElement {
+  static override styles = css`
+    ::slotted(.slide-hidden) {
+      display: none;
+    }
+  `;
+
+  @property({ type: Number }) slideIndex = 0;
+
+  // In video use @queryAssignedElements()
+  @queryAssignedNodes('', false, '*') private slideElements!: HTMLElement[];
+
   override render() {
-    return html`<h1>simple-carousel lives here!</h1>`
+    return html`<slot></slot>`;
   }
+
+  override firstUpdated() {
+    this.navigateSlide();
+  }
+
+  override updated() {
+    this.navigateSlide();
+  }
+
+  private navigateSlide() {
+    for (let i = 0; i < this.slideElements.length; i++) {
+      if (i === this.slideIndex) {
+        showSlide(this.slideElements[i]);
+      } else {
+        hideSlide(this.slideElements[i]);
+      }
+    }
+  }
+}
+
+function hideSlide(el: HTMLElement) {
+  el.classList.add('slide-hidden');
+}
+
+function showSlide(el: HTMLElement) {
+  el.classList.remove('slide-hidden');
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    "simple-carousel": SimpleCarousel;
+    'simple-carousel': SimpleCarousel;
   }
 }
-
-// document.createElement('simple-carousel')
